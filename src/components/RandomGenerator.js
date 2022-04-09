260f2f9cc0c25e13501854dca17ded4646f8a4c6
orBents <bentoesouza@hotmail.com>
2022-04-08 23:10:04 -0300

    first commit

---
+++ src/components/RandomGenerator.js
@@ -0,0 +1,82 @@
+class RandomGenerator {
+  constructor() {
+    const BASE_FREQUENCIES = {    //interval of a min/max bineural frequency dict base 
+      'alpha': [8, 13],
+      'beta': [13, 30],
+      'gamma': [30, 100]
+    };
+    this.BASE_FREQUENCIES = BASE_FREQUENCIES;
+
+    let currentFrequencies = {
+      'alpha': 0,
+      'beta': 0,
+      'gamma': 0
+    };
+    this.currentFrequencies = currentFrequencies;
+
+    let delay = 100; //1 second to update frequencies
+    this.delay = delay; 
+  }
+
+  //FREQUENCY
+
+  setBaseGen(currentFrequencies) {   //set random base to use in a incrementation
+    for (const frequency in this.currentFrequencies) {
+      currentFrequencies[frequency] = this.getRandomInterval(frequency);
+    }
+    return currentFrequencies;
+  }
+
+  setDirection() { //set direction to a random increment, decrement or sustain
+    return Math.floor(Math.random() * 3) - 1;
+  }
+
+  getRandomInterval(frequency) {   //get random base frequency in interval for each dict insert on atenuateContinue
+    let min = Math.ceil(this.BASE_FREQUENCIES[frequency][0]);
+    let max = Math.floor(this.BASE_FREQUENCIES[frequency][1]);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getRandFrequency() {    //get a random frequency by index to increment in atenuateContinue
+    let index = Math.floor(Math.random() * 3);
+    return Object.keys(this.currentFrequencies)[index];
+  }
+
+  atenuateContinueFrequency(currentFrequencies) {    //increment or decrement to a base frequency
+    setInterval(() => {
+      //let frequency = Math.floor(Math.random() * 3); //choose any base frequency to increment
+      let frequency = this.getRandFrequency();
+      let increment = this.setDirection();
+      //console.log(`frequency: ${frequency} ${currentFrequencies[frequency]} increment: ${increment} result: ${currentFrequencies[frequency] + increment}`);
+      currentFrequencies[frequency] += increment;
+    }, this.delay);
+  }
+
+  ///OFFSET
+
+  setRandomOffset() { //set random base offset
+    return Math.floor(Math.random() * 100) - 60;
+  }
+
+  //GAIN
+
+  setRandomGain() { //set random base offset
+    return Math.floor(Math.random());
+  }
+
+  //PLAY
+
+  play() {
+    this.currentFrequencies = this.setBaseGen(this.currentFrequencies);
+    this.atenuateContinueFrequency(this.currentFrequencies);
+  }
+}
+
+//TEST
+let generator = new RandomGenerator();
+generator.play();
+  //setRandomOffset();
+
+export {
+  RandomGenerator
+};
---
+++ src/components/audioGraph.js
@@ -0,0 +1,63 @@
+import { RandomGenerator } from './RandomGenerator';
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+const destination = audioContext.destination;
+let initialised = false;    // flag to store singleton state init
+
+class AudioGraph {
+    constructor() {
+        // each component will now have two oscillators,
+        // one playing the base frequency,
+        // and the other playing the offset frequency
+        this.state = {
+            randomGenerator: new RandomGenerator()
+        };
+        const baseNode = audioContext.createOscillator();
+        const beatNode = audioContext.createOscillator();
+        this.baseNode = baseNode;
+        this.beatNode = beatNode;
+
+        const lPanner = new StereoPannerNode(audioContext, {pan: -1});
+        baseNode.connect(lPanner);
+        const rPanner = new StereoPannerNode(audioContext, {pan: +1});
+        beatNode.connect(rPanner);
+        const gainNode = audioContext.createGain();  
+        this.gainNode = gainNode;  
+
+        lPanner.connect(gainNode);
+        rPanner.connect(gainNode);
+        gainNode.connect(destination);
+        
+        if(!initialised) {
+            audioContext.suspend(); // prevent the context from starting
+            initialised = true;
+        } 
+        baseNode.start();       // until a user action triggers it.
+        beatNode.start();
+    }
+    updateOscillators(frequency, offset) {
+        try {        
+            frequency = parseInt(frequency); 
+            offset = parseInt(offset);
+            if(Math.abs(frequency > 250)) frequency = 250;
+            if(Math.abs(offset) > 20) offset = Math.sign(offset);
+            this.baseNode.frequency.value = frequency; // use a number input, in hertz
+            this.beatNode.frequency.value = frequency + offset;  // use a range slider (+-60Hz)
+        } catch(e) {
+            audioContext.suspend();
+            throw e;
+        }
+        return {frequency, offset};
+    }    
+    changeGain(gain) {
+        
+        gain = parseFloat(gain);
+        if(gain < 0 || gain > 0.5) gain = 0.5;
+        return this.gainNode.gain.value = gain;  // use a volume slider for the entire component
+    }
+}
+
+export {
+   audioContext, AudioGraph
+};
